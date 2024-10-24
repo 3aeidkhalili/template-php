9af7be9ea71757d68041db02cc7a81dda1ff49cb
Saeed Khalili <154358632+3aeidkhalili@users.noreply.github.com>
2024-10-23 21:32:59 -0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,79 @@
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>اوریادوب | طراحی سایت</title>
+		<meta name="keywords" content="اوریادوب، طراحی سایت حرفه ای در ارومیه، طراحی گرافیک، طراحی اپلیکیشن اندروید و ios، تبلیغات در ارومیه، کارآفرینی، طراحی سایت در ارومیه"/>
+		<meta name="description" content="اوریادوب، طراحی سایت حرفه ای در ارومیه، طراحی گرافیک، طراحی اپلیکیشن اندروید و ios، تبلیغات در ارومیه، کارآفرینی، طراحی سایت در ارومیه">
+		<meta name="viewport" content="wclassth=device-wclassth, initial-scale=1.0">
+		<meta name="robots" content=" index , follow ">
+		<meta name="author" content="سعید خلیلی">
+		<link href="https://cdn.jsdelivr.net/gh/rastikerdar/sahel-font@v1.0.0-alpha23/dist/font-face.css" rel="stylesheet" type="text/css">
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+	</head>
+	<body>
+		<div class="sotoon">
+			<div class="navar-menu">
+				<div class="text-menu">
+					<p><a href="https://www.uryadweb.ir">اوریادوب | طراحی سایت</a></p>
+				</div>
+				<div class="list-menu">
+					<ul class="ulstyle">
+						<li><a href="index.php">&nbsp;خانه&nbsp;</a></li>
+						<li><a href="order.php">&nbsp;سفارش طراحی سایت&nbsp;</a></li>
+						<li><a href="samples.php">&nbsp;نمونه کار ها&nbsp;</a></li>
+						<li><a href="about.php">&nbsp;درباره ما&nbsp;</a></li>
+						<li><a href="contact.php">&nbsp;تماس با ما&nbsp;</a></li>
+					</ul>
+				</div>
+				<div class="data">
+					امروز <?php include_once('php/jdf.php');$out=jdate('l j F Y');echo $out;?>
+				</div>
+			</div>
+			<div class="postha">
+			<p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+
+</p>
+				<div class="from-order">
+					<div style="float: right;">
+						<form action="files/get_data8.php" class="input" method="POST">
+							<p>نام :</p> 
+							<input name="text" type="text" class="input" maxlength="5"/>
+							<p>نام خانوادگی :</p> 
+							<input name="text" type="text" class="input"/>
+							<p>نام پدر :</p>
+							<input name="text" type="text" class="input"/>
+							<p>کدملی :</p> 
+							<input name="text" type="text" class="input"/>
+							<p>شماره تماس :</p>  
+							<input style="text-align: left;" name="text" placeholder="0914"  class="input" required/>
+					</div>
+					<div class="form-order-fit">
+							<p>شماره همراه :</p>
+							<input name="phone" placeholder="044"  class="input"required/>
+							<p>ایمیل :</p>
+							<input name="email" placeholder="exmple@exmple.com"  class="input"required/>
+							<p>استان و شهرستان :</p>
+							<input name="world" placeholder="آذربایجان غربی / ارومیه"  class="input"required/><br/><br/>
+							<p>ارتباط با ما</p>
+							<select class="input">
+								<option>ارتباط با مدیریت سایت و پشتیبانی</option>
+
+							</select>
+					</div>
+					<div>
+													<p>توضیحات</p>
+							<textarea style="width: 551px;height: 202px;" class="input"></textarea>
+					</div>					
+							<br/><input type="submit" class="input" value="ارسال"/>
+							<input type="reset" class="input" value="ازنو"/>
+					</div>
+						</form>	
+						
+				</div>
+			</div>
+			<div class="footer">
+				<p style="padding: 12px 24px 0 0;color: white;text-align: center;">تمامی حقوق این سایت محفوظ سایت اوریاد وب می باشد. طراحی و کد نویسی : سعید خلیلی</p>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ samples.php
@@ -0,0 +1,43 @@
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>اوریادوب | طراحی سایت</title>
+		<meta name="keywords" content="اوریادوب، طراحی سایت حرفه ای در ارومیه، طراحی گرافیک، طراحی اپلیکیشن اندروید و ios، تبلیغات در ارومیه، کارآفرینی، طراحی سایت در ارومیه"/>
+		<meta name="description" content="اوریادوب، طراحی سایت حرفه ای در ارومیه، طراحی گرافیک، طراحی اپلیکیشن اندروید و ios، تبلیغات در ارومیه، کارآفرینی، طراحی سایت در ارومیه">
+		<meta name="viewport" content="wclassth=device-wclassth, initial-scale=1.0">
+		<meta name="robots" content=" index , follow ">
+		<meta name="author" content="سعید خلیلی">
+		<link href="https://cdn.jsdelivr.net/gh/rastikerdar/sahel-font@v1.0.0-alpha23/dist/font-face.css" rel="stylesheet" type="text/css">
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+	</head>
+	<body>
+		<div class="sotoon">
+			<div class="navar-menu">
+				<div class="text-menu">
+					<p><a href="https://www.uryadweb.ir">اوریادوب | طراحی سایت</a></p>
+				</div>
+				<div class="list-menu">
+					<ul class="ulstyle">
+						<li><a href="index.php">&nbsp;خانه&nbsp;</a></li>
+						<li><a href="order.php">&nbsp;سفارش طراحی سایت&nbsp;</a></li>
+						<li><a href="samples.php">&nbsp;نمونه کار ها&nbsp;</a></li>
+						<li><a href="about.php">&nbsp;درباره ما&nbsp;</a></li>
+						<li><a href="contact.php">&nbsp;تماس با ما&nbsp;</a></li>
+					</ul>
+				</div>
+				<div class="data">
+					امروز <?php include_once('php/jdf.php');$out=jdate('l j F Y');echo $out;?>
+				</div>
+			</div>
+			<div class="postha">
+				<div>
+					<p>هیچ نمونه کار موجود نیست</p>
+				</div>
+			</div>
+		</div>
+			<div class="footer">
+				<p style="padding: 12px 24px 0 0;color: white;text-align: center;">تمامی حقوق این سایت محفوظ سایت اوریاد وب می باشد. طراحی و کد نویسی : سعید خلیلی</p>
+			</div>
+		</div>
+	</body>
+</html>
